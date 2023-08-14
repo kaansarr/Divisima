f00b6eeb0735e63d21490c57cf2701b1a7767d67
kaansarr <kaansar33@gmail.com>
2023-08-14 17:18:06 +0300

    business layer

---
+++ Divisima.BL/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisima.BL.Repositories
+{
+    public interface IRepository<T>
+    {
+        public IQueryable<T> GetAll();//IQueryable, Icollection, IList,List: IEnumerable
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> exp);
+        public T GetBy(Expression<Func<T, bool>> exp);
+        public Task Add(T entity);
+        public Task Update(T entity);
+        public Task Update(T entity, params Expression<Func<T, object>>[] expressions);
+        public Task Delete(T entity);
+        public Task DeleteRange(IEnumerable<T> entities);
+    }
+}
+//CRUD
